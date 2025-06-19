16ef4f3ff8440ad25fbc52d2bf074bc64a8cfb5f
T-S-4256 <sp4256824@gmail.com>
2025-06-20 00:33:00 +0530

    One Data Added

---
+++ Updated_Gordiodrilus_elegans.php
@@ -0,0 +1,14 @@
+<h2><em>Gordiodrilus elegans </em><span>Beddard, 1892</span></h2>
+<ul class="list-4">
+  <li>Slender, small worms, size 35–47 mm.</li>
+  <li>Setae lumbricine, closely paired.</li>
+  <li>Dorsal pores absent.</li>
+  <li>Spermathecal pores two pairs, on the intersegmental regions of 7/8 and 8/9, at setal line of b.</li>
+  <li>Male genital region protruded in the form of either a nearly square area or a dumbbell-shaped area; male pores are seemingly situated at the intersegmental furrow of xvii/xviii.</li>
+  <li>Spermathecae two pairs, without discrete diverticula.</li>
+  <li>Gizzard, typhlosole, and supra-intestinal glands absent.</li>
+  <li>A ventromedian calciferous gland in ix.</li>
+  <li>Intestinal origin in xii.</li>
+  <li>Single species recorded in India.</li>
+  <li>Type locality Lagos, Nigeria and is exotic.</li>
+</ul>
