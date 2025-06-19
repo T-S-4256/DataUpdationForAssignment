3036fe422a08e057e6e23cc6622107863cda0f02
T-S-4256 <sp4256824@gmail.com>
2025-06-19 23:29:32 +0530

    One Data Added

---
+++ Update_Metaphire.php
@@ -0,0 +1,84 @@
+<tr>
+    <td>1.</td>
+    <td><em>andamanensis</em></td>
+    <td>North Cinque Island, Andaman and Nicobar Islands, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>2.</td>
+    <td><em>anomala</em></td>
+    <td>Botanical garden, Sibpur, West Bengal, India</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>3.</td>
+    <td><em>bahli</em></td>
+    <td>Colombo, Sri Lanka</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>4.</td>
+    <td><em>birmanica</em></td>
+    <td>Bhamo, Myanmar</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>5.</td>
+    <td><em>californica</em></td>
+    <td>Sausolita Bay, California, USA</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>6.</td>
+    <td><em>churachandpurensis</em></td>
+    <td>Churachandpur, Manipur, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>7.</td>
+    <td><em>harrietensis</em></td>
+    <td>Mount Harriet, Andaman and Nicobar Islands, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>8.</td>
+    <td><em>houlleti</em></td>
+    <td>Calcutta (Kolkata), West Bengal, India</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>9.</td>
+    <td><em>manipurensis</em></td>
+    <td>Lamdeng Makhaleikai Forest range, Manipur, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>10.</td>
+    <td><em>peguana</em></td>
+    <td>Rangoon, Myanmar</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>11.</td>
+    <td><em>planata</em></td>
+    <td>Rangoon, Myanmar</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>12.</td>
+    <td><em>posthuma</em></td>
+    <td>Java, Indonesia</td>
+    <td>Exotic</td>
+</tr>
+<tr>
+    <td>13.</td>
+    <td><em>scitula</em></td>
+    <td>Port Blair, Andaman and Nicobar Islands, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>14.</td>
+    <td><em>thabiensis</em></td>
+    <td>Yangoupokpi-lokchao wildlife Sanctuary, Tengnoupal, Manipur, India</td>
+    <td>Endemic</td>
+</tr>
