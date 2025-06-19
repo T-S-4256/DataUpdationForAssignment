0e30a3167d298bdf1736cae570464d8c23b1d758
T-S-4256 <sp4256824@gmail.com>
2025-06-19 23:56:31 +0530

    One Data Added

---
+++ update_tonoscolex.php
@@ -0,0 +1,48 @@
+<tr>
+    <td>1.</td>
+    <td><em>horai</em></td>
+    <td>Cherrapunji, Meghalaya, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>2.</td>
+    <td><em>indicus</em></td>
+    <td>Siki, Arunachal Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>3.</td>
+    <td><em>kabakensis</em></td>
+    <td>Kabak, Arunachal Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>4.</td>
+    <td><em>kalimpongensis</em></td>
+    <td>Lava in Neora Valley National Park, West Bengal, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>5.</td>
+    <td><em>michaelseni</em></td>
+    <td>Wakro, Arunachal Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>6.</td>
+    <td><em>monarchis</em></td>
+    <td>Darjeeling to Soom (Ghoom), West Bengal, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>7.</td>
+    <td><em>oneili</em></td>
+    <td>Janakmukh, Arunachal Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>8.</td>
+    <td><em>striatus</em></td>
+    <td>Rotung (Rottung), Arunachal Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
