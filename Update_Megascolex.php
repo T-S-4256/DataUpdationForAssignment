09ca3b04429f679835e841db214e1ef173eda85a
T-S-4256 <sp4256824@gmail.com>
2025-06-19 23:22:29 +0530

    One Data Added

---
+++ Update_Megascolex.php
@@ -0,0 +1,210 @@
+<tr>
+    <td>1.</td>
+    <td><em>auriculata</em></td>
+    <td>Vandiperiyar, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>2.</td>
+    <td><em>avicula</em></td>
+    <td>Peermade (Peerumedu), India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>3.</td>
+    <td><em>cochinensis cochinensis</em></td>
+    <td>Forest Tramway, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>4.</td>
+    <td><em>cochinensis phaseolus</em></td>
+    <td>Parambikulum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>5.</td>
+    <td><em>eunephrus</em></td>
+    <td>Coorloon, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>6.</td>
+    <td><em>filiciseta</em></td>
+    <td>Parambikulum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>7.</td>
+    <td><em>hendersoni</em></td>
+    <td>Tiger shola near Kodaikanal, Tamil Nadu, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>8.</td>
+    <td><em>imperatrix</em></td>
+    <td>Lamb’s Rock–Coonoor, Tamil Nadu, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>9.</td>
+    <td><em>insignis</em></td>
+    <td>Trivandrum (Thirvananthapuram), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>10.</td>
+    <td><em>jamiesoni</em></td>
+    <td>Jeypore Ghati around 10 km away from Jeypore town, Odisha, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>11.</td>
+    <td><em>jeyporeghatiensis</em></td>
+    <td>Jeypore Ghati around 10 km away from Jeypore town, Odisha, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>12.</td>
+    <td><em>kavalaianus</em></td>
+    <td>Kavalai, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>13.</td>
+    <td><em>konkanensis</em></td>
+    <td>North Konkan, Maharashtra, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>14.</td>
+    <td><em>konkanensis longus</em></td>
+    <td>Parambikulum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>15.</td>
+    <td><em>lawsoni</em></td>
+    <td>Ootacamund (Udagamandalam or Ooty), Tamil Nadu, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>16.</td>
+    <td><em>papparensis</em></td>
+    <td>Mannoorkara close to Peppara Wildlife Sanctuary, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>17.</td>
+    <td><em>peermadensis</em></td>
+    <td>Peermade (Peerumedu), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>18.</td>
+    <td><em>pheretima</em></td>
+    <td>Manakoti, Karnataka, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>19.</td>
+    <td><em>polytheca polytheca</em></td>
+    <td>Kavalai in forest tramway, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>20.</td>
+    <td><em>polytheca uniquus</em></td>
+    <td>Kumili, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>21.</td>
+    <td><em>polytheca zonatus</em></td>
+    <td>Parambikulum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>22.</td>
+    <td><em>porphyrozonus</em></td>
+    <td>Nandi Hills, Karnataka, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>23.</td>
+    <td><em>pumilio</em></td>
+    <td>Trivandrum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>24.</td>
+    <td><em>quadripapillatus</em></td>
+    <td>Rani Duduma around 25 km away from Semiliguda town, Odisha, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>25.</td>
+    <td><em>ratus</em></td>
+    <td>Coorloon, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>26.</td>
+    <td><em>travancorensis travancorensis</em></td>
+    <td>Pallode (Palode), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>27.</td>
+    <td><em>travancorensis bonaccordensis</em></td>
+    <td>Bonaccord (Bonacaud, Bonakkad), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>28.</td>
+    <td><em>travancorensis ghatensis</em></td>
+    <td>Maddathoray (Madathara), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>29.</td>
+    <td><em>travancorensis pentagonalis</em></td>
+    <td>Trivandrum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>30.</td>
+    <td><em>travancorensis proboscidea</em></td>
+    <td>Tenmalai, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>31.</td>
+    <td><em>travancorensis quilonensis</em></td>
+    <td>Shasthancottah (Sathamkotta), Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>32.</td>
+    <td><em>triangularis</em></td>
+    <td>Kavalai, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>33.</td>
+    <td><em>tripartitus</em></td>
+    <td>Tirumalai Hills, Andhra Pradesh, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>34.</td>
+    <td><em>trivandranus</em></td>
+    <td>Trivandrum, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
+<tr>
+    <td>35.</td>
+    <td><em>vazhichlensis</em></td>
+    <td>Mannoorkara close to Peppara Wildlife Sanctuary, Kerala, India</td>
+    <td>Endemic</td>
+</tr>
